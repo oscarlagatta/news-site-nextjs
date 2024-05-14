71a9cde2afc54ad9fa48d7922cd9a60966dcf708
Oscar <oscarlagatta@gmail.com>
2024-05-14 08:06:26 +0100

    initial commit

---
+++ app/(content)/archive/layout.js
@@ -0,0 +1,14 @@
+import {useIntersection} from "next/dist/client/use-intersection";
+
+export default function ArchiveLayout({archive, latest}) {
+    return (
+        <div><h1>News Archive</h1>
+            <section id='archive-filter'>
+                {archive}
+            </section>
+            <section id='archive-latest'>
+                {latest}
+            </section>
+        </div>
+    )
+}
