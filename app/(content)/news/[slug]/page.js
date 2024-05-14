52a75c8453c756c5914f67ab624a22c1e401c2f6
Oscar Lagatta <oscar.lagatta@whistl.co.uk>
2024-05-14 10:29:15 +0100

    Update better-sqlite3 to v9.6.0 and refactor NewsPage component
    The package better-sqlite3 in both "backend/package.json" and "backend/package-lock.json" has been updated from "^9.4.3" to "^9.6.0". The NewsPage component in "app/(content)/news/page.js" has been refactored to use the "getAllNews" function from "@/lib/news". The outdated useEffect hook and related state variables have been removed. Various other dependencies in "package-lock.json" have been updated.

--- app/(content)/news/[slug]/page.js
+++ app/(content)/news/[slug]/page.js
@@ -1,10 +1,10 @@
-import {DUMMY_NEWS} from "@/dummy-news";
 import NotFound from "@/app/(content)/not-found";
 import Link from "next/link";
+import {getNewsItem} from "@/lib/news";
 
-export default function NewsDetailPage({params}) {
+export default async function NewsDetailPage({params}) {
     const newsSlug = params.slug
-    const newsItem = DUMMY_NEWS.find((newsItem => newsItem.slug === newsSlug));
+    const newsItem = await getNewsItem(newsSlug);
 
     if (!newsItem) {
         return NotFound();
